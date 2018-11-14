6855f6eceff754935a908d5952efb3a4467260aa
Muhammad Jalloh <techbuktu@jalloh.com>
2018-11-13 21:17:36 -0500

    Installed npm packages for @angular/material use

--- src/app/modules/material/material.module.ts
+++ src/app/modules/material/material.module.ts
@@ -1,10 +1,30 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {MatButtonModule, MatCardModule, MatToolbarModule, MatIconModule, 
+  MatGridListModule, MatListModule} from '@angular/material';
 
+@NgModule({
+  imports: [CommonModule,
+    MatButtonModule, MatCardModule, 
+    MatToolbarModule, MatIconModule, 
+    MatGridListModule, MatListModule
+  ],
+      
+  exports: [
+    MatButtonModule, MatCardModule, 
+    MatToolbarModule, MatIconModule, 
+    MatGridListModule, MatListModule],
+
+    declarations: [
+      
+    ]
+})
+/*
 @NgModule({
   imports: [
     CommonModule
   ],
   declarations: []
 })
+*/
 export class MaterialModule { }
