68faedcd86a627ef4bc9b0220e202a96e50a0d15
Muhammad Jalloh <techbuktu@jalloh.com>
2018-11-13 22:00:58 -0500

    BlogService fully implemented for interfacing with the RESTful APIs

--- src/app/services/blog/blog.service.ts
+++ src/app/services/blog/blog.service.ts
@@ -1,9 +1,41 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ApiRootService } from './api-root.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BlogService {
+  blogApiUrl:string;
+  blog_api_endpoint:string = "/api/blogger/blogs/";
 
-  constructor() { }
+  constructor(private Http: HttpClient, private ApiRoot: ApiRootService) { 
+    this.blogApiUrl = this.ApiRoot.getRoot() + this.blog_api_endpoint;
+  }
+
+  getAllBlogs(){
+   return this.Http.get(this.blogApiUrl);
+  }
+
+  getBlogByUrl(blog_url){
+    return this.Http.get(this.blogApiUrl + blog_url);
+  }
+
+  createBlog(new_blog_data){
+    return this.Http.post(this.blogApiUrl, new_blog_data);
+  }
+
+  updateBlog(blog_url, updated_blog_data){
+    return this.Http.put(this.blogApiUrl, blog_url, updated_blog_data);
+  }
+
+  deleteBlog(blog_url){
+    return this.Http.delete(this.blogApiUrl + blog_url);
+  }
+
+  getBlogsByBlogger(bloggerUrl){
+    return this.Http.get(this.blogApiUrl + '?blogger__link=' + bloggerUrl);
+  }
+  
+
 }
