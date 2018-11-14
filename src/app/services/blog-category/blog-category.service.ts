d40438334ee1438b91f310c545a2af120863eb1c
Muhammad Jalloh <techbuktu@jalloh.com>
2018-11-13 22:04:09 -0500

    BlogCategoryService done to make CRUD calls with the REST APIs

--- src/app/services/blog-category/blog-category.service.ts
+++ src/app/services/blog-category/blog-category.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ApiRootService } from './api-root.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BlogCategoryService {
+  categoryApiUrl: string;
+  blog_category_api:string = "/api/blogger/blogcategories/";
 
-  constructor() { }
+  constructor(private http:HttpClient, private ApiRoot: ApiRootService) {
+    this.categoryApiUrl = this.ApiRoot.getRoot() + this.blog_category_api;
+   }
+
+  getCategoriesByBlog(blogUrl){
+    return this.http.get(this.categoryApiUrl+ '?blog__page='+blogUrl);
+  }
+
+  getCategoryByUrl(categoryUrl){
+    return this.http.get(this.categoryApiUrl+categoryUrl);
+  }
+
+  createCategory(new_category_data){
+    return this.http.post(this.categoryApiUrl, new_category_data);
+  }
+
+  updateCategory(categoryUrl, updated_category_data){
+    return this.http.put(this.categoryApiUrl+categoryUrl, updated_category_data);
+  }
+
+  deleteCategory(categoryUrl){
+    return this.http.delete(this.categoryApiUrl+categoryUrl);
+  }
 }
