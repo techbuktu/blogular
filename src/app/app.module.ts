c74fc2fe2298dfe9d09002f6a0c00505716794c3
Muhammad Jalloh <techbuktu@jalloh.com>
2018-11-21 18:08:50 -0500

    Created a fresh routing module in /app/modules/routing to handle Blogular app's inter-component routing. Adjusted AppModule class accordingly

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AppRoutingModule } from './app-routing.module';
+//import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './modules/routing/routing.module';
 import { MaterialModule } from './modules/material/material.module';
 import { AppComponent } from './app.component';
 import { BlogListComponent } from './components/blog-list/blog-list.component';
