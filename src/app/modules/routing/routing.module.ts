b95347c6338faca5c01a2c9eadb759351c7b9ace
Muhammad Jalloh <techbuktu@jalloh.com>
2018-11-21 18:30:16 -0500

    Imported all app's components into routing module ... ready for use.

--- src/app/modules/routing/routing.module.ts
+++ src/app/modules/routing/routing.module.ts
@@ -1,6 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+//Import all component classes for use in routing requests
+import { BlogCategoryDetailComponent } from '../../components/blog-category-detail/blog-category-detail.component';
+import { BlogDetailComponent } from '../../components/blog-detail/blog-detail.component';
+import { BlogListComponent } from '../../components/blog-list/blog-list.component';
+import { BloggerDetailComponent } from '../../components/blogger-detail/blogger-detail.component';
+import { BloggerListComponent } from '../../components/blogger-list/blogger-list.component';
+import { BlogpostComponent } from '../../components/blogpost/blogpost.component';
+import { HomeComponent } from '../../components/home/home.component';
+
 const routes: Routes = [];
 
 @NgModule({
