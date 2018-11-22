3e38ef1f30b22f1e8dba048516b7020d2e09db35
Muhammad Jalloh <techbuktu@jalloh.com>
2018-11-22 01:49:13 -0500

    Implemented BlgpostComponent's component class logic

--- src/app/components/blogpost/blogpost.component.ts
+++ src/app/components/blogpost/blogpost.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { BlogPostService } from '../../services/blog-post/blog-post.service';
+import { BLOG_POST } from '../../mock-data/blog_post';
+
 
 @Component({
   selector: 'app-blogpost',
@@ -6,10 +9,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./blogpost.component.css']
 })
 export class BlogpostComponent implements OnInit {
+  
 
-  constructor() { }
+  constructor(private blogPostService: BlogPostService) { }
+
+  
+  blog_post;
+  post_error_message:string;
 
   ngOnInit() {
+    this.getBlogPost();
   }
-
+  
+  getBlogPost(){
+    this.blogPostService.getPost('digital-nomad').
+    subscribe(
+      res => {
+        this.blog_post = res;
+      },
+      err => {
+        this.post_error_message = err.message;
+        this.blog_post = BLOG_POST;
+      },
+      () => {
+        console.log("BlogComponent.getBlogPosts() done running..");
+      }
+    )
+  }
 }
